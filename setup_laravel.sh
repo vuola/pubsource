dbf2ae63b2c444c089143d8830f35237b9228728
vuola <markus.vuolahti@gmail.com>
2023-07-22 17:50:16 +0300

    setup as one command

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # create DB_HOST
-mariadb_host="${MARIADB_SERVICE_HOST}"
+mariadb_host="${MARIADB_SERVICE_HOST}" && \
 
 # create APP_URL
-node_name="${NODE_NAME}.local"
-node_url="http://$node_name"
+node_name="${NODE_NAME}.local" && \
+node_url="http://$node_name" && \
 
 # run replacements
-sed "s|MARIADB_HOST|$mariadb_host|g; s|NODE_URL|$node_url|g" .env.example > .env
+sed "s|MARIADB_HOST|$mariadb_host|g; s|NODE_URL|$node_url|g" .env.example > .env && \
 
 # create APP_KEY
 php artisan key:generate
