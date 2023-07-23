462a3fa4d56bdf4f30118b34cd16e7850e24ec81
vuola <markus.vuolahti@gmail.com>
2023-07-23 10:04:50 +0300

    tweak

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -4,20 +4,20 @@ while true; do
   if [ -e /ready ]; then
     echo "File /ready found. Running ./setup_laravel.sh..."
 
-        rm /ready
+    rm -f /ready
 
-        # create DB_HOST
-        mariadb_host="${MARIADB_SERVICE_HOST}"
+    # create DB_HOST
+    mariadb_host="${MARIADB_SERVICE_HOST}"
 
-        # create APP_URL
-        node_name="${NODE_NAME}.local"
-        node_url="http://$node_name"
+    # create APP_URL
+    node_name="${NODE_NAME}.local"
+    node_url="http://$node_name"
 
-        # run replacements
-        sed "s|MARIADB_HOST|$mariadb_host|g; s|NODE_URL|$node_url|g" .env.example > .env
+    # run replacements
+    sed "s|MARIADB_HOST|$mariadb_host|g; s|NODE_URL|$node_url|g" .env.example > .env
 
-        # generate APP_KEY
-        php artisan key:generate
+    # generate APP_KEY
+    php artisan key:generate
 
     break
   else
