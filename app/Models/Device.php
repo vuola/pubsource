8ecaeb7397d796adbcff191ad75c890ac4e496f8
vuola <markus.vuolahti@gmail.com>
2023-08-07 10:04:48 +0300

    Kalliola August 1-7 contributions

---
+++ app/Models/Device.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ProductCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Device extends Model
+{
+    use HasFactory;
+}
